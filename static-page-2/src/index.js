ca234cf26b84e7494065cc4e612c5d001303d7e0
javadebadi <javad.ebadi.1990@gmail.com>
2022-03-03 20:08:21 +0330

    Added component to build the static page

--- static-page-2/src/index.js
+++ static-page-2/src/index.js
@@ -2,19 +2,53 @@ import React from "react"
 import ReactDOM from "react-dom"
 import logo from "./logo.svg"
 
-const page = (
+function Header() {
+  return (
+    <nav>
+      <img src={logo} width="100px"  alt="react-logo" />
+    </nav>
+  )
+}
+
+function Title() {
+  return (
+    <h1> React features </h1>
+  )
+}
+
+function Content() {
+  return (
   <div>
-    <img src={logo} width="100px"  alt="react-logo" />
-    <h1>React features</h1>
     <ul>
       <li>First created at 2013</li>
       <li>Is maintained by Facebook</li>
       <li>Powered thousands of enterprise apps, including mobile apps</li>
     </ul>
   </div>
-)
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>Javad Ebadi, All rights are reservered.</small>
+    </footer>
+  )
+}
+
+
+function Page() {
+  return (
+    <div>
+    <Header />
+    <Title />
+    <Content />
+    <Footer />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  page,
+  <Page />,
   document.getElementById("root")
 )
